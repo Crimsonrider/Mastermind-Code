3b81967c32a1f0c2b9ace1d02c9fb65ec2208665
urmilalchandani <urmilalchandani@deepaks-mbp>
2017-05-10 19:19:11 -0700

    Added Javadoc comments on Marble and Peg class.

--- Mastermind/src/Marble.java
+++ Mastermind/src/Marble.java
@@ -1,29 +1,42 @@
 public class Marble extends GamePiece {
-	private boolean matched;
-	private int position;
+	private boolean matched; // variable for the color of the guessed marble
+	private int position; // position of the marble
 
+	/**
+	 * This constructor creates a marble with the desired color and position
+	 * 
+	 * @param color
+	 *            - color of the new marble
+	 * @param position
+	 *            - position of the marble
+	 */
 	public Marble(String color, int position) {
 		this.color = color;
 		this.position = position;
 		matched = false;
 	}
 
+	/**
+	 * This accessor method returns if the marble already has a match
+	 * 
+	 * @return true if the marble already has a match, false otherwise
+	 */
 	public boolean hasBeenMatched() {
 		return matched;
 	}
 
+	/**
+	 * This method sets matched to true.
+	 */
 	public void hasAMatch() {
 		matched = true;
 	}
 
-	public int getPosition() {
-		return position;
-	}
-
-	public void setPosition(int pos) {
-		position = pos;
-	}
-
+	/**
+	 * Returns the color of the marble
+	 * 
+	 * @return color
+	 */
 	public String toString() {
 		return color;
 	}
--- Mastermind/src/Peg.java
+++ Mastermind/src/Peg.java
@@ -1,13 +1,23 @@
 
 public class Peg extends GamePiece {
+	/**
+	 * Constructor creates a new peg object with the desired color
+	 * @param c - color
+	 */
 	public Peg(String c) {
 		color = c;
 	}
-	
+
+	/**
+	 * checks if the peg is white
+	 */
 	public boolean isWhite() {
 		return (getColor().equals("white"));
 	}
 
+	/**
+	 * @return the color of the peg
+	 */
 	public String toString() {
 		return color;
 	}
